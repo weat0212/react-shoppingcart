c136088a52c721413edad330162bb15e497341e8
LAPTOP-DSGKT2L5\88693 <amo6324@yahoo.com.tw>
2023-08-29 23:23:58 +0800

    新增更改購物車CSS

--- frontend/src/layout/CartSideBar.js
+++ frontend/src/layout/CartSideBar.js
@@ -1,37 +1,58 @@
 import "../styles/CartSideBar.css";
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const CartSideBar = (props) => {
-    const { navVisible, cart } = props;
-    const navContainerClass = `nav-container ${navVisible ? 'showNav' : 'hideNav'} ${!navVisible ? 'hidden' : ''}`;
-    console.log(cart)
-    return(
-        <>
-            <div className={navContainerClass}>
-                {/* Your navigation content goes here */}
-                {cart.length ?
-                    cart?.map((item, index) => {
-                        return (
-                            <>
-                            <li key={index}>
-                                <div>{item.name}</div>
-                                <div>${item.price}</div>
-                                <div>x {item.quantity}</div>
-                                <div>${item.price * item.quantity}</div>
-                            </li>
-                            <button>
-                                <Link to="Cart">訂單結帳</Link>
-                            </button>
-                            </>
-                        );}
-                    ) :
-                    <div>目前購物車為空</div>
-                }
+  const { navVisible, cart } = props;
+  const navContainerClass = `nav-container ${
+    navVisible ? "showNav" : "hideNav"
+  } ${!navVisible ? "hidden" : ""}`;
+  console.log(cart);
+  return (
+    <>
+      <div className={navContainerClass}>
+        {cart.length ? (
+          <ul className="ulcss">
+            {cart.map((item, index) => (
+              <div>
+                <li key={index} className="list-item">
+                  <img
+                    className="d-flex justify-content-center"
+                    style={{
+                      width: "80px",
+                      height: "50px",
+                      objectFit: "cover",
+                    }}
+                    src={item?.link}
+                    alt=""
+                  />
+                  <div>商品:{item.name}</div>
+                  <div>
+                    NT${item.price} x {item.quantity}
+                  </div>
 
-            </div>
-        </>
-    )
-}
+                  <div>總計NT${item.price * item.quantity}</div>
+                </li>
+                <hr />
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div>目前購物車為空</div>
+        )}
+
+        {cart.length > 0 && (
+          <div className="cart-chkt-btn-cont">
+            <button>
+              <Link className="link-without-underline" to="Cart">
+                訂單結帳
+              </Link>
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default CartSideBar;
