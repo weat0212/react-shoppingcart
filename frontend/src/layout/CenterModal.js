941642ccdcd5815e18b99859a5cee29693b1154c
LAPTOP-DSGKT2L5\88693 <amo6324@yahoo.com.tw>
2023-08-29 23:23:58 +0800

    新增更改購物車CSS

--- frontend/src/layout/CenterModal.js
+++ frontend/src/layout/CenterModal.js
@@ -3,23 +3,26 @@ import {Button, Modal} from "react-bootstrap";
 
 export function CenterModal(props) {
 
-    const {title = '提示訊息', content = '系統忙碌中，請稍後再試...', btnLeft = '確認', btnRight, children} = props
+    const {title = '加入購物車', content = '系統忙碌中，請稍後再試...', btnLeft = '確認', btnRight, children} = props
 
     return (
         <Modal {...props} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
                     {title}
+                  
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 {children || <p>{content}</p>}
+                <img src={Modal.item?.link} alt="" />
             </Modal.Body>
             <Modal.Footer>
                 <Button variant={'secondary'} onClick={props.onHide}>{btnLeft}</Button>
                 {
                     btnRight && <Button variant={'secondary'} onClick={props.onHide}>{btnRight}</Button>
                 }
+               
             </Modal.Footer>
         </Modal>
     )
